6ceb293823a18076da89137e262b77deabe0ac27
prayogartp <prayogaprayoga446@gmail.com>
2026-09-23 22:41:17 +0700

    Latihan Pemrograman Web 2

---
+++ pert 6/latihan2.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+<?php
+$x=array("one","two","three");
+foreach ($x as $value)
+{
+ echo $value . "<br />";
+}
+
+$b["sayur"] ="wortel";
+$b["daging"] ="ayam";
+$b["utama"] ="nasi";
+$jumlah = sizeof($b);
+print "Jumlah array b = $jumlah <br>";
+// variabel $jumlah akan bernilai 4
+?>
+</body>
+</html>
